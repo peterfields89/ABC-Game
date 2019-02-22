242b387118a2016f73f8598edd7f1e8c838cb198
Peter van der Velde <peterjavait@gmail.com>
2019-02-22 15:25:15 +0100

    Spel ABC met if els

---
+++ ABC.java
@@ -0,0 +1,223 @@
+import java.util.Scanner;
+
+public class ABC {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welkom bij het spel ABC, wat is uw naam?");
+		String naamIn = scanner.nextLine();
+		System.out.println("Hey " + naamIn + (", laten we het spel starten! Wat is het eerste letter van het alfabet?"));
+		String ingegevene = scanner.nextLine();
+		if (ingegevene.equals("a")) {
+			System.out.println("Heel goed " + naamIn + "! Het eerste letter van het alfabet is: a");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene + " is niet goed. Start het spel opnieuw op :)");
+		}
+		
+		String ingegevene2 = scanner.nextLine();
+		if (ingegevene2.equals("b")) {
+			System.out.println("Heel goed " + naamIn + "! Het tweede letter van het alfabet is: b");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene2 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene3 = scanner.nextLine();
+		if (ingegevene3.equals("c")) {
+			System.out.println("Heel goed " + naamIn + "! Het derde letter van het alfabet is: c");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene3 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene4 = scanner.nextLine();
+		if (ingegevene4.equals("d")) {
+			System.out.println("Heel goed " + naamIn + "! Het vierde letter van het alfabet is: d");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene4 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene5 = scanner.nextLine();
+		if (ingegevene5.equals("e")) {
+			System.out.println("Heel goed " + naamIn + "! Het vijfde letter van het alfabet is: e");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene5 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene6 = scanner.nextLine();
+		if (ingegevene6.equals("f")) {
+			System.out.println("Heel goed " + naamIn + "! Het zesde letter van het alfabet is: f");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene6 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene7 = scanner.nextLine();
+		if (ingegevene7.equals("g")) {
+			System.out.println("Heel goed " + naamIn + "! Het zevende letter van het alfabet is: g");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene7 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene8 = scanner.nextLine();
+		if (ingegevene8.equals("h")) {
+			System.out.println("Heel goed " + naamIn + "! Het achste letter van het alfabet is: h");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene8 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene9 = scanner.nextLine();
+		if (ingegevene9.equals("i")) {
+			System.out.println("Heel goed " + naamIn + "! Het negende letter van het alfabet is: i");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene9 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene10 = scanner.nextLine();
+		if (ingegevene10.equals("j")) {
+			System.out.println("Heel goed " + naamIn + "! Het tiende letter van het alfabet is: j");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene10 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene11 = scanner.nextLine();
+		if (ingegevene11.equals("k")) {
+			System.out.println("Heel goed " + naamIn + "! Het elfde letter van het alfabet is: k");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene11 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene12 = scanner.nextLine();
+		if (ingegevene12.equals("l")) {
+			System.out.println("Heel goed " + naamIn + "! Het twaalfde letter van het alfabet is: l");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene12 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene13 = scanner.nextLine();
+		if (ingegevene13.equals("m")) {
+			System.out.println("Heel goed " + naamIn + "! Het dertiende letter van het alfabet is: m");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene13 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene14 = scanner.nextLine();
+		if (ingegevene14.equals("n")) {
+			System.out.println("Heel goed " + naamIn + "! Het viertiende letter van het alfabet is: n");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene14 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene15 = scanner.nextLine();
+		if (ingegevene15.equals("o")) {
+			System.out.println("Heel goed " + naamIn + "! Het vijftiende letter van het alfabet is: o");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene15 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene16 = scanner.nextLine();
+		if (ingegevene16.equals("p")) {
+			System.out.println("Heel goed " + naamIn + "! Het zestiende letter van het alfabet is: p. Dit is de eerste letter van de maker van dit spel ;)");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene16 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene17 = scanner.nextLine();
+		if (ingegevene17.equals("q")) {
+			System.out.println("Heel goed " + naamIn + "! Het zeventiende letter van het alfabet is: q");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene17 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		
+		String ingegevene18 = scanner.nextLine();
+		if (ingegevene18.equals("r")) {
+			System.out.println("Heel goed " + naamIn + "! Het achtiende letter van het alfabet is: r");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene18 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene19 = scanner.nextLine();
+		if (ingegevene19.equals("s")) {
+			System.out.println("Heel goed " + naamIn + "! Het negentiende letter van het alfabet is: s");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene19 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene20 = scanner.nextLine();
+		if (ingegevene20.equals("t")) {
+			System.out.println("Heel goed " + naamIn + "! Het twintigste letter van het alfabet is: t");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene20 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene21 = scanner.nextLine();
+		if (ingegevene21.equals("u")) {
+			System.out.println("Heel goed " + naamIn + "! Het een-en-twintigste letter van het alfabet is: u");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene21 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene22 = scanner.nextLine();
+		if (ingegevene22.equals("v")) {
+			System.out.println("Heel goed " + naamIn + "! Het twee-en-twintigste letter van het alfabet is: v");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene22 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene23 = scanner.nextLine();
+		if (ingegevene23.equals("w")) {
+			System.out.println("Heel goed " + naamIn + "! Het drie-en-twintigste letter van het alfabet is: w");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene23 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene24 = scanner.nextLine();
+		if (ingegevene24.equals("x")) {
+			System.out.println("Heel goed " + naamIn + "! Het vier-en-twintigste letter van het alfabet is: x");
+			System.out.println("Wat is het volgende letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene24 + " is niet goed. Start het spel opnieuw op :)");
+		}
+		String ingegevene25 = scanner.nextLine();
+		if (ingegevene25.equals("y")) {
+			System.out.println("Heel goed " + naamIn + "! Het vijf-en-twintigste letter van het alfabet is: y");
+			System.out.println("Wat is het laatste letter van het alfabet? ");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene25 + " is niet het laatste letter van het alfabet. Start het spel opnieuw op :)");
+		}
+		String ingegevene26 = scanner.nextLine();
+		if (ingegevene26.equals("z")) {
+			System.out.println("Heel goed " + naamIn + "! x is de laatste letter van het alfabet!");
+			System.out.println("Je bent een topper " + naamIn + "! Je weet nu alle 26 letters van het alfabet! Bedankt voor het spelen :)");
+				}
+		else {
+			System.out.println("Helaas, " + ingegevene26 + " is niet goed. Start het spel opnieuw op :)");
+		}		
+	}
+}
+			
+	
+
